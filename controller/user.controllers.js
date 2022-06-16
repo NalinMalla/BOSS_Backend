629cd7e5864998213716a27540b2b8d621dd9998
NalinMalla <nilanallam@gmail.com>
2022-06-16 13:30:01 +0545

    First Commit

---
+++ controller/user.controllers.js
@@ -0,0 +1,81 @@
+let User = require('../models/user.model');
+
+const findAllUsers = (req, res) => {
+    User.find()
+        .then( users => res.json(users))
+        .catch( err => res.status(400).json(`Error: ${err}`));
+};
+
+const createUser = (req, res) => {
+    const firstName = req.body.firstName;
+    const middleName = req.body.middleName;
+    const lastName = req.body.lastName;
+    // res.json(req.body);
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const newUser = new User({
+        // userName: req.body.userName,
+        userName: {
+            firstName, middleName, lastName
+        }, 
+        email: email,
+        password: password, 
+    });
+
+    newUser.save()
+        .then( () => res.json(`User ${firstName} Added.`))
+        .catch( err => res.status(400).json(`Error: ${err}`));
+};
+
+const findUserById = (req, res) => {
+    User.findById(req.params.id)
+        .then( user => 
+            user === null ? 
+                res.status(404).json(`User with Id ${req.params.id} does not exists.`) : 
+                res.json(user)
+        )
+        .catch( err => res.status(400).json(`Error: ${err}`));
+};
+
+const findUserByEmail = (req, res) => {
+    User.findOne({email: req.params.email})
+        .then( user => 
+            user === null ? 
+                res.status(404).json(`User with Email ${req.params.email} does not exists.`) : 
+                res.json(user)
+        )
+        .catch( err => res.status(400).json(`Error: ${err}`));
+};
+
+const deleteUser = (req, res) => {
+    User.findByIdAndDelete(req.params.id)
+        .then( () => res.json(`User ${req.params.id} deleted.`))
+        .catch( err => res.status(400).json(`Error: ${err}`));
+};
+
+const updateUser = (req, res) => {
+    User.findById(req.params.id)
+        .then( user => {
+            firstName = req.body.firstName;
+            middleName = req.body.middleName;
+            lastName = req.body.lastName;
+            user.name = {firstName, middleName, lastName}
+            user.email = req.body.email;
+            user.password = req.body.password;
+
+            user.save()
+                .then(() => res.json(`User ${req.params.id} updated.`))
+                .catch( err => res.status(400).json(`Error: ${err}`));
+        })
+        .catch( err => res.status(400).json(`Error: ${err}`));
+};
+
+exports.findAllUsers = findAllUsers;
+exports.createUser = createUser;
+exports.findUserById = findUserById;
+exports.findUserByEmail = findUserByEmail;
+exports.deleteUser = deleteUser;
+exports.updateUser = updateUser;
+
+
---
+++ models/user.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema;
+
+const UserSchema = new schema(
+  {
+    userName: {
+      firstName: {
+        type: String,
+        required: [true, "First Name is mandatory."],   // Or just required: true, works as well, if you want to forgo the error messages.
+        minlength: [1, "First Name cannot be blank."],
+        maxlength: [32, "First Name must be 32 characters at maximum."],
+    },
+      middleName: {
+        type: String,   
+        maxlength: [32, "Middle Name must be 32 characters at maximum."],
+    },
+      lastName: {
+        type: String,
+        required: [true, "Last Name is mandatory."],
+        minlength: [1, "Last Name cannot be blank."],
+        maxlength: [32, "Last Name must be 32 characters at maximum."],
+    },
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
